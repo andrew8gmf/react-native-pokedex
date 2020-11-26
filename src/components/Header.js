9718b5b971a2d21c5b8c0121b83b3768eb85d3ee
andrew8gmf <andrew8gmf@gmail.com>
2020-11-26 15:56:40 -0300

    update: header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Dimensions, View, Text } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Dimensions, View, TextInput, Text } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faBars, faFilter, faStar as fasFaStar, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farFaStar } from '@fortawesome/free-regular-svg-icons';
 
+const winWidth = Dimensions.get('window').width;
 const winHeight = Dimensions.get('window').height;
 
 export default function Header() {
@@ -11,8 +12,16 @@ export default function Header() {
         <SafeAreaView>
             <StatusBar backgroundColor="black"/>
             <View style={styles.header}>
-                <View>
-                    <Text style={styles.headerText}>Pokédex</Text>
+                <View style={styles.headerBar}>
+                    <View style={styles.searchBar}>
+                        <FontAwesomeIcon icon={ faBars } color='white' />
+                        <TextInput style={styles.search} placeholder='Search' placeholderTextColor='white'></TextInput>
+                    </View>
+                    <View style={styles.icons}>
+                        <FontAwesomeIcon icon={ faFilter } color='white' />
+                        <FontAwesomeIcon icon={ fasFaStar } color='white' />
+                        <FontAwesomeIcon icon={ faEllipsisV } color='white' />
+                    </View>            
                 </View>
             </View>
         </SafeAreaView>
@@ -21,17 +30,41 @@ export default function Header() {
 
 const styles = StyleSheet.create({
     header: {
-        width: '100%',
+        width: winWidth,
         height: winHeight * 0.1,
+        position: 'absolute',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'red',
     },
-    headerText: {
+    headerBar: {
+        width: '92%',
+        height: '50%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        backgroundColor: 'black',
+        borderRadius: 5,
+    },
+    searchBar: {
+        width: '65%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    search: {
+        width: '100%',
+        marginLeft: '8%',
         fontWeight: 'bold',
-        fontSize: 20,
-        color: '#333',
+        fontSize: 16,
+        color: 'white',
         letterSpacing: 1,
     },
+    icons: {
+        width: '20%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }, 
 })
