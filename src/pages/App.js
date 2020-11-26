e7fcc6971075b9d01ec88de1290220fc789e178e
andrew8gmf <andrew8gmf@gmail.com>
2020-11-25 21:27:06 -0300

    fix: semicolons usage

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,33 +5,33 @@ import PokemonList from '../components/PokemonList';
 import Pagination from '../components/Pagination';
 
 export default function App() {
-  const [pokemon, setPokemon] = useState([])
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
-  const [nextPageUrl, setNextPageUrl] = useState()
-  const [prevPageUrl, setPrevPageUrl] = useState()
-  const [loading, setLoading] = useState(true)
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true)
-    let cancel
+    setLoading(true);
+    let cancel;
     axios.get(currentPageUrl, {
       cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
-      setLoading(false)
-      setNextPageUrl(res.data.next)
-      setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map(p => p.name))
-    })
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
+      setPokemon(res.data.results.map(p => p.name));
+    });
 
-    return () => cancel()
-  }, [currentPageUrl])
+    return () => cancel();
+  }, [currentPageUrl]);
 
   function gotoNextPage() {
-    setCurrentPageUrl(nextPageUrl)
+    setCurrentPageUrl(nextPageUrl);
   }
 
   function gotoPrevPage() {
-    setCurrentPageUrl(prevPageUrl)
+    setCurrentPageUrl(prevPageUrl);
   }
 
   if (loading) {
