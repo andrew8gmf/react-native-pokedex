b2a0218575dec524226a8794add3b2e0e0c94ca7
andrew8gmf <andrew8gmf@gmail.com>
2020-11-26 16:29:26 -0300

    update: adjust pokemonlist margin

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Dimensions, View, Text } from 'react-native';
+
+const winWidth = Dimensions.get('window').width;
+const winHeight = Dimensions.get('window').height;
 
 export default function PokemonList({ pokemon }) {
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={{ width: winWidth, height: winHeight * 0.1 }}/>
       {pokemon.map(p => (
         <Text key={p}>{p}</Text>
       ))}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: winWidth,
+    height: winHeight * 0.8,
+  }
+})
