f26d2e2b936cb015fbcf193378519857ebf1a444
andrew8gmf <andrew8gmf@gmail.com>
2020-11-28 20:04:28 -0300

    update: flatlist best practices

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Dimensions, View, FlatList, Image, Text } from 'react-native';
 
 const winWidth = Dimensions.get('window').width;
 const winHeight = Dimensions.get('window').height;
 
 export default function PokemonList({ pokemon }) {
+
+  renderRow = ({ item }) => {
+    const imageUrl = 'https://pokeres.bastionbot.org/images/pokemon/';
+    const pokemonUrl = item.url;
+    const pokemonId = pokemonUrl.split('https://pokeapi.co/api/v2/pokemon/');
+    const imageLink = imageUrl + pokemonId[1].substring(0, pokemonId[1].length - 1) + ".png";
+
+    return (
+      <View style={styles.item}>
+        <Image
+          style={styles.image}
+          resizeMode="contain"
+          source={{ uri: imageLink }}
+        />
+        <Text style={styles.text}>{item.name}</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
         data={pokemon}
-        keyExtractor={(item, index) => index}
+        keyExtractor={(item, index) => index.toString()}
         numColumns={2}
-        renderItem={({ item }) => {
-          const imageUrl = 'https://pokeres.bastionbot.org/images/pokemon/';
-          const pokemonUrl = item.url;
-          const pokemonId = pokemonUrl.split('https://pokeapi.co/api/v2/pokemon/');
-          const imageLink = imageUrl + pokemonId[1].substring(0, pokemonId[1].length - 1) + ".png";
-
-          return (
-            <View style={styles.item}>
-              <Image
-                style={styles.image}
-                resizeMode="contain"
-                source={{ uri: imageLink }}
-              />
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
-          );
-        }}
+        renderItem={renderRow}
       />
     </View>
   );
